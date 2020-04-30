dbb1fdd898b462a0c1d6a51a13408bc2c42bcd70
oniolalekan <olalekan.joseph@gmail.com>
2020-04-30 19:36:01 +0100

    add serializers file and update model

--- src/zeno/models.py
+++ src/zeno/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class ZenoItem(models.Model):
-  id = models.IntegerField(primary_key = True) 
+  zenoid = models.IntegerField(null=True, blank=True) 
   timestamp = models.CharField(max_length=100, null=True, blank=True)
   temperature = models.FloatField(null=True, blank=True)
   duration = models.CharField(max_length=250, null=True, blank=True)
--- src/zeno/views.py
+++ src/zeno/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from zeno.models import ZenoItem
+from zeno.serializers import ZenoItemSerializer
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.reverse import reverse
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 # Create your views here.
+class ZenoItemViewSet(viewsets.ModelViewSet):
+  queryset = ZenoItem.objects.all()
+  serializer_class = ZenoItemSerializer
+
+  def perform_create(self, serializer):
+    # Save instance to get primary key and then update URL
+    instance = serializer.save()
+    instance.url = reverse('zenoitem-detail', args=[instance.pk], request=self.request)
+    instance.save()
+
+  # Deletes all todo items
+  def delete(self, request):
+    ZenoItem.objects.all().delete()
+    return Response(status=status.HTTP_204_NO_CONTENT) 
